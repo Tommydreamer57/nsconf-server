e6f74160ac4091bb263848e69aa72406d72d3fd5
Thomas Lowry <thomas@lowrysoftware.com>
2020-02-17 17:39:40 -0700

    added social & session feedback links

--- feedback/feedback.js
+++ feedback/feedback.js
@@ -16,5 +16,15 @@ module.exports = {
             description: "Sign up to volunteer for North Star International.",
             url: "",
         },
+        {
+            title: "Social Event",
+            description: "Provide feedback about a social event at the conference this year.",
+            url: "https://forms.gle/bRqtNpW3CrMjbTLP6",
+        },
+        {
+            title: "Keynote/Breakout",
+            description: "Provide feedback about a keynote/breakout session at the conference this year.",
+            url: "https://forms.gle/YWmmiHBHJbSDY7dT7",
+        },
     ],
 };
